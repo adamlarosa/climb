71be3517cc255896bb5b5af26a8c32720af5f309
Adam La Rosa <adam.larosa@gmail.com>
2020-06-18 22:52:57 -0700

    environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 require 'pry'
 require 'sqlite3'
+require 'json'
+require 'rest-client'
+require 'nokogiri'
+require 'open-uri'
 
 require_relative '../lib/route.rb'
 require_relative '../lib/menu.rb'
