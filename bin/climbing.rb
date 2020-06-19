c62e7cf4adc095ee6cf825713a4ffae4522fa6de
Adam La Rosa <adam.larosa@gmail.com>
2020-06-18 23:08:06 -0700

    runfile start

--- bin/climbing.rb
+++ bin/climbing.rb
@@ -1,4 +1,13 @@
 require_relative '../config/environment.rb'
 
-binding.pry
-p
+m = Menu.new
+
+input = m.prompt
+while input.downcase != "x" do
+	if input == "help"
+	    puts "HELP!"
+	elsif input == "pry"
+	    binding.pry
+	end
+	input = m.prompt
+end
