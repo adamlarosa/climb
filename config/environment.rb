c2ce995f50d7ac3630344039b97d5afdb25a6bc1
Adam La Rosa <adam.larosa@gmail.com>
2020-06-18 22:09:35 -0700

    menus init

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 require 'pry'
 require 'sqlite3'
+
 require_relative '../lib/route.rb'
+require_relative '../lib/menu.rb'
 
 DB = {:conn => SQLite3::Database.new("db/climbing.db")}
---
+++ lib/menu.rb
@@ -0,0 +1,6 @@
+class Menu
+
+	def prompt
+	    gets.chomp
+	end
+end
